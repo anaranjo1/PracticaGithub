4ba9a0fcdbddc3ee2b104efe0128cbec027b5bd6
jbril98 <myEmail@example.com>
2022-11-21 20:28:45 +0100

    fijo otra semilla y modifico paréntesis en la suma

--- PracticaGithub/practica.py
+++ PracticaGithub/practica.py
@@ -3,12 +3,12 @@
 
 import numpy as np
 
-np.random.seed(1)
+np.random.seed(4)
 
 x = np.random.randint(10)
 y = np.random.randint(10)
 z = np.random.randint(10)
-suma = x+(y+z)
+suma = (x+y)+z
 
 print("'x' es igual a: " + str(x) + "\n'y' es igual a: " + str(y) + "\n'z' es igual a: " + str(z))
 print("La suma total de estos números es: " + str(suma))
